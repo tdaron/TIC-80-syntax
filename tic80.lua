1fe42e3030c48828848d8bdcbc971d9caed60cdc
hayattgd <56343984+hayattgd@users.noreply.github.com>
2024-10-21 16:58:18 +0900

    removed return, added deprecated function and replaced integer to colors or button id or something...

--- tic80.lua
+++ tic80.lua
@@ -25,21 +25,47 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 --]]
 
+---@diagnostic disable
+---@class color_0-15
+---@class button_0-31
+---@class key_1-94
+---@class sprite_0-511
+---@class flag_0-7
+---@class bank_0-7
+---@class vbank_0-1
+---@class sfx_0-63
+---@class music_0-7
+---@class channel_0-3
+---@class note_0-95
+---@class volume_0-15
+---@class speed_-4-3
+---@class flip_0-3
+---@class rotate_0-3
+---@class texsrc_0-3
+---@class index_0-255
+---@class bits_1_2_4_8
+---@class val32_0-4294967295
+---@class val8_0-255
+
 ---This function allows you to read the status of TIC's controller buttons.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/btn)
----@param id integer
+---
+---[View key map](https://github.com/nesbox/TIC-80/wiki/Key-Map)
+---@param id button_0-31
 ---@return boolean is_pressed
-function btn(id) return false end
+function btn(id) end
 
 ---This function allows you to read the status of one of TIC's buttons.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/btnp)
----@param id integer
+---
+---[View key map](https://github.com/nesbox/TIC-80/wiki/Key-Map)
+---@param id button_0-31
 ---@param hold? integer
 ---@param period? integer
 ---@return boolean is_pressed
-function btnp(id, hold, period) return false end
+function btnp(id, hold, period) end
 
 ---This function draws a filled circle of the desired <strong>radius</strong> and <strong>color</strong> with its center at <strong>x</strong>, <strong>y</strong>.
 ---
@@ -47,7 +73,7 @@ function btnp(id, hold, period) return false end
 ---@param x integer
 ---@param y integer
 ---@param radius integer
----@param color integer
+---@param color color_0-15
 function circ(x, y, radius, color) end
 
 ---Draws the circumference of a circle with its center at <strong>x</strong>, <strong>y</strong> using the <strong>radius</strong> and <strong>color</strong> requested.
@@ -56,7 +82,7 @@ function circ(x, y, radius, color) end
 ---@param x integer
 ---@param y integer
 ---@param radius integer
----@param color integer
+---@param color color_0-15
 function circb(x, y, radius, color) end
 
 ---This function limits drawing to a clipping region or 'viewport' defined by <code>x</code>,<code>y</code>, <code>width</code>, and <code>height</code>.
@@ -71,27 +97,31 @@ function clip(x, y, width, height) end
 ---This function clears/fills the entire screen using <strong>color</strong>.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/cls)
----@param color? integer
+---@param color? color_0-15
 function cls(color) end
 
 ---This function draws a filled ellipse centered at <strong>x, y</strong> using palette index <strong>color</strong> and radii <strong>a</strong> and <strong>b</strong>.
+---
+---(added in 0.9)
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/elli)
 ---@param x integer
 ---@param y integer
 ---@param a integer
 ---@param b integer
----@param color integer
+---@param color color_0-15
 function elli(x, y, a, b, color) end
 
 ---This function draws an ellipse border with the radiuses <strong>a</strong> <strong>b</strong> and <strong>color</strong> with its center at <strong>x</strong>, <strong>y</strong>.
+---
+---(added in 0.9)
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/ellib)
 ---@param x integer
 ---@param y integer
 ---@param a integer
 ---@param b integer
----@param color integer
+---@param color color_0-15
 function ellib(x, y, a, b, color) end
 
 ---This function causes program execution to be terminated <strong>after</strong> the current <code>TIC</code> function ends.
@@ -105,9 +135,11 @@ function exit() end
 ---@param sprite_id integer
 ---@param flag integer
 ---@return boolean
-function fget(sprite_id, flag) return false end
+function fget(sprite_id, flag) end
 
 ---This function sets the sprite flag to a given boolean value.
+---
+---(added in 0.8)
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/fset)
 ---@param sprite_id integer
@@ -117,25 +149,27 @@ function fset(sprite_id, flag, bool) end
 
 ---This function will draw text to the screen using the foreground spritesheet as the font.
 ---
+---(added in 0.8)
+---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/font)
 ---@param text string
 ---@param x integer
 ---@param y integer
----@param transcolor? integer
+---@param transcolor? color_0-15
 ---@param char_width? integer
 ---@param char_height? integer
 ---@param fixed? boolean
 ---@param scale? integer
 ---@param alt? boolean
 ---@return integer text_width
-function font(text, x, y, transcolor, char_width, char_height, fixed, scale, alt) return 0 end
+function font(text, x, y, transcolor, char_width, char_height, fixed, scale, alt) end
 
 ---The function returns <em>true</em> if the key denoted by <em>keycode</em> is pressed otherwise it returns <strong>false</strong>.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/key)
 ---@param code? integer
 ---@return boolean is_pressed
-function key(code) return false end
+function key(code) end
 
 ---This function returns <code>true</code> if the given key is pressed but wasn't pressed in the previous frame.
 ---
@@ -144,7 +178,7 @@ function key(code) return false end
 ---@param hold? integer
 ---@param period? integer
 ---@return boolean pressed
-function keyp(code, hold, period) return false end
+function keyp(code, hold, period) end
 
 ---Draws a straight line from point (x0,y0) to point (x1,y1) in the specified color.
 ---
@@ -153,7 +187,7 @@ function keyp(code, hold, period) return false end
 ---@param y0 integer
 ---@param x1 integer
 ---@param y1 integer
----@param color integer
+---@param color color_0-15
 function line(x0, y0, x1, y1, color) end
 
 ---The map consists of cells of 8x8 pixels, each of which can be filled with a tile using the <a href="https://github.
@@ -167,7 +201,7 @@ function line(x0, y0, x1, y1, color) end
 ---@param sy? integer
 ---@param colorkey? integer
 ---@param scale? integer
----@param remap? integer
+---@param remap? function
 function map(x, y, w, h, sx, sy, colorkey, scale, remap) end
 
 ---This function copies a continuous block of <a href="RAM">RAM</a> from one address to another.
@@ -182,7 +216,7 @@ function memcpy(to, from, length) end
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/memset)
 ---@param addr integer
----@param value integer
+---@param value val8_0-255
 ---@param length integer
 function memset(addr, value, length) end
 
@@ -192,7 +226,7 @@ function memset(addr, value, length) end
 ---@param x integer
 ---@param y integer
 ---@return integer tile_id
-function mget(x, y) return 0 end
+function mget(x, y) end
 
 ---This function writes the specified background tile <strong>tile_id</strong> into the map at the given position.
 ---
@@ -217,70 +251,76 @@ function mouse() return 0, 0, false, false, false, 0, 0 end
 ---This function starts playing a <strong>track</strong> created in the <a href="https://github.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/music)
----@param track integer
----@param frame integer
----@param row integer
----@param loop boolean
----@param sustain boolean
----@param tempo integer
----@param speed integer
+---@param track? integer
+---@param frame? integer
+---@param row? integer
+---@param loop? boolean
+---@param sustain? boolean
+---@param tempo? integer
+---@param speed? integer
 function music(track, frame, row, loop, sustain, tempo, speed) end
 
 ---This function allows you to read directly from RAM.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/peek)
 ---@param addr integer
----@param bits? integer
+---@param bits? bits_1_2_4_8
 ---@return integer value
-function peek(addr, bits) return 0 end
+function peek(addr, bits) end
+
+---This function allows you to read directly from RAM.
+---
+---(added in 1.0)
+---
+---[View documents](https://github.com/nesbox/TIC-80/wiki/peek)
+---@param addr integer
+---@return integer value
+function peek1(addr) end
+
+---This function allows you to read directly from RAM.
+---
+---(added in 1.0)
+---
+---[View documents](https://github.com/nesbox/TIC-80/wiki/peek)
+---@param addr integer
+---@return integer value
+function peek2(addr) end
 
 ---This function allows you to read directly from RAM.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/peek)
 ---@param addr integer
 ---@return integer value
-function peek1(addr) return 0 end
-
----This function allows you to read directly from RAM.
----
----[View documents](https://github.com/nesbox/TIC-80/wiki/peek)
----@param addr integer
----@return integer value
-function peek2(addr) return 0 end
-
----This function allows you to read directly from RAM.
----
----[View documents](https://github.com/nesbox/TIC-80/wiki/peek)
----@param addr integer
----@return integer value
-function peek4(addr) return 0 end
+function peek4(addr) end
 
 ---This function can read or write individual pixel color values.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/pix)
 ---@param x integer
 ---@param y integer
----@param color? integer
----@return integer color
-function pix(x, y, color) return 0 end
+---@param color? color_0-15
+---@return color_0-15 color
+function pix(x, y, color) end
 
 ---The name "pmem" means <strong>persistent memory</strong>.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/pmem)
----@param index integer
----@param val32 integer
----@return integer val32
-function pmem(index, val32) return 0 end
+---@param index index_0-255
+---@param val32 val32_0-4294967295
+---@return val32_0-4294967295 val32
+function pmem(index, val32) end
 
 ---This function allows you to write directly to <a href="https://github.com/nesbox/TIC-80/wiki/RAM">RAM</a>.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/poke)
 ---@param addr integer
 ---@param val integer
----@param bits? integer
+---@param bits? bits_1_2_4_8
 function poke(addr, val, bits) end
 
 ---This function allows you to write directly to <a href="https://github.com/nesbox/TIC-80/wiki/RAM">RAM</a>.
+---
+---(added in 1.0)
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/poke)
 ---@param addr integer
@@ -288,6 +328,8 @@ function poke(addr, val, bits) end
 function poke1(addr, val) end
 
 ---This function allows you to write directly to <a href="https://github.com/nesbox/TIC-80/wiki/RAM">RAM</a>.
+---
+---(added in 1.0)
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/poke)
 ---@param addr integer
@@ -307,12 +349,12 @@ function poke4(addr, val) end
 ---@param text integer
 ---@param x integer
 ---@param y integer
----@param color integer
+---@param color color_0-15
 ---@param fixed boolean
 ---@param scale integer
 ---@param smallfont boolean
 ---@return integer width
-function print(text, x, y, color, fixed, scale, smallfont) return 0 end
+function print(text, x, y, color, fixed, scale, smallfont) end
 
 ---This function draws a filled rectangle at the specified position.
 ---
@@ -321,7 +363,7 @@ function print(text, x, y, color, fixed, scale, smallfont) return 0 end
 ---@param y integer
 ---@param width integer
 ---@param height integer
----@param color integer
+---@param color color_0-15
 function rect(x, y, width, height, color) end
 
 ---This function draws a one pixel thick rectangle border.
@@ -331,7 +373,7 @@ function rect(x, y, width, height, color) end
 ---@param y integer
 ---@param width integer
 ---@param height integer
----@param color integer
+---@param color color_0-15
 function rectb(x, y, width, height, color) end
 
 ---Resets the TIC virtual "hardware" and immediately restarts the cartridge.
@@ -342,24 +384,24 @@ function reset() end
 ---This function will play the sound with <strong>id</strong> created in the SFX Editor.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/sfx)
----@param id integer
----@param note integer
----@param duration integer
----@param channel integer
----@param volume integer
----@param speed integer
+---@param id sfx_0-63
+---@param note? note_0-95
+---@param duration? integer
+---@param channel? channel_0-3
+---@param volume? volume_0-15
+---@param speed? speed_-4-3
 function sfx(id, note, duration, channel, volume, speed) end
 
 ---Draws the <a href="sprite">sprite</a> number <strong>index</strong> at the <strong>x</strong> and <strong>y</strong> <a href="coordinate">coordinate</a>.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/spr)
----@param id integer
+---@param id sprite_0-511
 ---@param x integer
 ---@param y integer
----@param colorkey? integer
+---@param colorkey? color_0-15
 ---@param scale? integer
----@param flip? integer
----@param rotate? integer
+---@param flip? flip_0-3
+---@param rotate? rotate_0-3
 ---@param w? integer
 ---@param h? integer
 function spr(id, x, y, colorkey, scale, flip, rotate, w, h) end
@@ -368,11 +410,13 @@ function spr(id, x, y, colorkey, scale, flip, rotate, w, h) end
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/sync)
 ---@param mask? integer
----@param bank? integer
+---@param bank? bank_0-7
 ---@param tocart? boolean
 function sync(mask, bank, tocart) end
 
----This function draws a triangle filled with texture from either <code>SPRITES</code> or <code>MAP</code> <a href="ram">RAM</a> or VBANK.
+---This function draws a triangle filled with texture from either <code>SPRITES</code> or <code>MAP</code> <a href="https://github.com/nesbox/TIC-80/wiki/RAM">RAM</a> or VBANK.
+---
+---(added in 1.0)
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/ttri)
 ---@param x1 integer
@@ -387,27 +431,49 @@ function sync(mask, bank, tocart) end
 ---@param v2 integer
 ---@param u3 integer
 ---@param v3 integer
----@param texsrc? integer
----@param chromakey? integer
+---@param texsrc? texsrc_0-3
+---@param chromakey? color_0-15
 ---@param z1? integer
 ---@param z2? integer
 ---@param z3? integer
 function ttri(x1, y1, x2, y2, x3, y3, u1, v1, u2, v2, u3, v3, texsrc, chromakey, z1, z2, z3) end
 
+---This function draws a triangle filled with texture from either <code>SPRITES</code> or <code>MAP</code> <a href="https://github.com/nesbox/TIC-80/wiki/RAM">RAM</a>.
+---
+---[View documents](https://github.com/nesbox/TIC-80/wiki/textri)
+---@param x1 integer
+---@param y1 integer
+---@param x2 integer
+---@param y2 integer
+---@param x3 integer
+---@param y3 integer
+---@param u1 integer
+---@param v1 integer
+---@param u2 integer
+---@param v2 integer
+---@param u3 integer
+---@param v3 integer
+---@param use_map? boolean
+---@param trans? color_0-15
+---@deprecated ttri
+function textri(x1, y1, x2, y2, x3, y3, u1, v1, u2, v2, u3, v3, use_map, trans) end
+
 ---This function returns the number of <em>milliseconds</em> elapsed since the cartridge began execution.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/time)
 ---@return number ticks
-function time() return 0 end
+function time() end
 
 ---This is a service function, useful for debugging.
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/trace)
 ---@param message string
----@param color? integer
+---@param color? color_0-15
 function trace(message, color) end
 
 ---This function draws a triangle filled with <strong>color</strong>, using the supplied vertices.
+---
+---(added in 0.9)
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/tri)
 ---@param x1 integer
@@ -416,10 +482,12 @@ function trace(message, color) end
 ---@param y2 integer
 ---@param x3 integer
 ---@param y3 integer
----@param color integer
+---@param color color_0-15
 function tri(x1, y1, x2, y2, x3, y3, color) end
 
 ---This function draws a triangle border with <strong>color</strong>, using the supplied vertices.
+---
+---(added in 0.9)
 ---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/trib)
 ---@param x1 integer
@@ -428,17 +496,105 @@ function tri(x1, y1, x2, y2, x3, y3, color) end
 ---@param y2 integer
 ---@param x3 integer
 ---@param y3 integer
----@param color integer
+---@param color color_0-15
 function trib(x1, y1, x2, y2, x3, y3, color) end
 
 ---This function returns the number of <em>seconds</em> elapsed since January 1st, 1970.
 ---
+---(added in 0.8)
+---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/tstamp)
 ---@return integer timestamp
-function tstamp() return 0 end
+function tstamp() end
 
 ---VRAM is double-banked, such that the entire 16kb VRAM address space can be "swapped" at any time between banks 0 and 1.
 ---
+---(added in 1.0)
+---
 ---[View documents](https://github.com/nesbox/TIC-80/wiki/vbank)
----@param id integer
+---@param id vbank_0-1
 function vbank(id) end
+--------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--https://github.com/nesbox/TIC-80/wiki/BDR
+BDR()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--https://github.com/nesbox/TIC-80/wiki/BOOT
+BOOT()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--https://github.com/nesbox/TIC-80/wiki/MENU
+MENU()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--https://github.com/nesbox/TIC-80/wiki/TIC
+TIC()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
